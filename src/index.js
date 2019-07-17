c1caca6496ec7b92b0ab7d2a60539fd7d52601ea
Kevin Oliveira <kevinobruno@gmail.com>
2019-07-16 20:48:27 -0400

    build: Configura Kue no projeto

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-// const to = require('await-to-js').default;
-// const scrapper = require('./scrapper');
-
-// const testUrl = 'https://sistemas.sefaz.am.gov.br/nfceweb/consultarNFCe.jsp?p=13190622991939001250650100001444511002312092|2|1|1|E878E999081D177A356C287BF7B524FAFAEC544D';
-// const testUrl = 'https://sistemas.sefaz.am.gov.br/nfceweb/consultarNFCe.jsp?p=13190622991939001250650100001444521008671237|2|1|1|3ECECC3E6D890D373E891308A4247CFCF2202E21';
-// const testUrl = 'http://sistemas.sefaz.am.gov.br/nfceweb/consultarNFCe.jsp?p=13190710713693000113650350004638841567420423|2|1|1|A2520183E9E9FF4F7D1C934B3C3066CD4547CEFC';
-// const testUrl = 'http://sistemas.sefaz.am.gov.br/nfceweb/consultarNFCe.jsp?p=13190711102673000179652050000268061097056853|2|1|0|cfe83cc2fdd1b6c52cb28fb42bbf03d68757381e';
-
-// scrapInvoiceUrl = async (url) => {
-//   const [err, result] = await to(scrapper(url));
-//   console.log(err);
-//   console.log(result);
-// };
-
-// scrapInvoiceUrl(testUrl);
-
+const kue = require('kue');
 const server = require('./server');
-server.listen(process.env.PORT || 3031);
+
+server.listen(process.env.PORT || 3000);
+kue.app.listen(process.env.KUE_UI_PORT || 12058);
